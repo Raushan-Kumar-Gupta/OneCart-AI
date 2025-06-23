b4ce81508d56b4d2763fc7997a7d396bb8295f91
Raushan-Kumar-Gupta <raushankalwar98242.com>
2025-06-24 02:12:58 +0530

    Add regiter, login, logout,  Vite for React

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,22 @@
 import express from 'express';
 let app = express();
 import dotenv from 'dotenv';
+import connectDb from './config/db.js';
+import cookieParser from 'cookie-parser';
+import authRoutes from './routes/authRoutes.js';
 dotenv.config();
 let port = process.env.PORT || 6000;
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/auth",authRoutes)
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${port}`)
+  connectDb()
 });
